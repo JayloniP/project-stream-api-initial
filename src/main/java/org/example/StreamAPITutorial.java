11902380d5cf60789fc8c93b5ebbf28b5614bc73
JayloniP <jay2161266@maricopa.edu>
2023-10-05 18:32:14 -0700

    Tutorial

--- src/main/java/org/example/StreamAPITutorial.java
+++ src/main/java/org/example/StreamAPITutorial.java
@@ -1,7 +1,8 @@
 package org.example;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class StreamAPITutorial {
 
@@ -20,7 +21,46 @@ public class StreamAPITutorial {
         // C1
         // C2
 
-        // TODO add more tutorial code here
+        //stream.noneMatch(s -> true);   // exception ,error- java: cannot find symbol symbol:variable stream
+        class Bar {
+            String name;
+
+            Bar(String name) {
+                this.name = name;
+            }
+        }
+
+        class Foo {
+            String name;
+            List<Bar> bars = new ArrayList<>();
+
+            Foo(String name) {
+                this.name = name;
+            }
+        }
+
+        IntStream.range(1, 4)
+                .mapToObj(i -> new Foo("Foo" + i))
+                .peek(f -> IntStream.range(1, 4)
+                        .mapToObj(i -> new Bar("Bar" + i + " <- "+ f.name))
+                        .forEach(f.bars::add))
+                .flatMap(f -> f.bars.stream())
+                .forEach(b -> System.out.println(b.name));
+
+        class Inner {
+            String foo;
+        }
+
+        class Nested {
+            Inner inner;
+        }
+
+        class Outer {
+            Nested nested;
+        }
+
+
+
 
     }
 }
